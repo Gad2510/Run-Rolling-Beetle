86141684cb6e18dbf8109643e75c37a2f1cdda3b
Azael Sánchez <azaelsnz@gmail.com>
2020-03-09 15:10:03 -0600

    Mapa y menu UI
    esta el mapa con 9 planos de difernte color y el menu ui con botones y se manejan las escenas

---
+++ Assets/SceneManage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+    //Funcionpara cambiar a pel juego
+      public  void Chargelevel(string level)
+    {
+        SceneManager.LoadScene (level);
+    }
+
+
+      public void ChargeOptions(string options)
+      {
+        SceneManager.LoadScene(options);
+      }
+    
+
+}
